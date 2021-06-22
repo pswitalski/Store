010ab76f72c2d4ebf7e315eda07a37c2a7b94006
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-22 15:54:46 +0200

    create GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+
 import Header from 'components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+        <GlobalStyle />
+        <Header />
     </div>
   );
 }
