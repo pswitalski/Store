e7bfba18ab22e24f382cd3c1964b5964cb650c13
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-22 16:12:48 +0200

    create theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
 
 import Header from 'components/Header';
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={theme} >
         <GlobalStyle />
         <Header />
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Header = () => (
-    <header>header</header>
+    <header>
+        header
+    </header>
 )
 
 export default Header;
